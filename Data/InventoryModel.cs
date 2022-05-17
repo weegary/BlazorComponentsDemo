ea90f7eba31a4d3915ac897d3901b20951f9c309
Gary Wee <wgary111@hotmail.com>
2022-05-18 04:50:01 +0800

    Add project files.

---
+++ Data/InventoryModel.cs
@@ -0,0 +1,14 @@
+﻿namespace ComponentsDemo.Data
+{
+    public class InventoryModel
+    {
+        public string StockName { get; set; }
+        public string StockID { get; set; }
+        public DateTime BuyDate { get; set; }
+        public int BuyVolume { get; set; }
+        public double BuyPrice { get; set; }
+        public string BrokerName { get; set; }
+        public double MarketPrice { get; set; }
+        public string Type { get; set; }
+    }
+}
